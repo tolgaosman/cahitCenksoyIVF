7c1b044b0030680b71717587ca185160ce4f50dd
tolgaosman <tolman2002@gmail.com>
2026-05-16 21:21:28 +0300

    s

--- translations.js
+++ translations.js
@@ -2,26 +2,26 @@
 const procDescriptions = {
     1: {
         title: "Ön Görüşme",
-        desc: "Tüp bebek tedavisinde, tedavi seçeneği ne olursa olsun öncelikle anne ve baba adayının detaylı bilgileri alınır. Bu bilgilere ek olarak öncesinde yapılmış olan tüm ameliyat, görüntüleme yöntemleri (rahim filmi gibi..), kan ve sperm test sonuçları değerlendirilir. Bu bilgilerin analiz edilmesi tüp bebek başarısında hayati önem taşır. Çifte talep ettikleri tedavi sorulur."
+        desc: ""
     },
     2: {
         title: "Tedavinin Planlanması",
-        desc: "Mevcut veriler ışığında uygun tedavi planlanır. Kıbrıs’ta tüp bebek tedavisi başlamadan önce, ön hazırlığın çiftin yaşadığı ülkede yapılması seyahat süresini kısaltır. Zaman kısıtlaması olmayan veya tedavi sürecinde Kıbrıs’ta tatil planlayan çiftlerin tüm süreci burada geçirmesi tercih edilir. Planlanan tedavi Kıbrıs’ta yumurta donasyonu ise anne adayının hormon düzeyleri adetinin 2. veya 3. günü kontrol edilir. Uygun düzeyde ise rahim duvarı hazırlığına başlanır. Rahim duvarı kalınlığı uygun seviyeye ulaştığı zaman progesteron hormonu tedavisi başlanması gerekir. Progesteron başlanacağı gün eşinin de sperm vermesi gerektiğinden çiftin Kıbrıs’ta olması gerekir. Baba adayı sperm örneğini daha önceden vermiş ise anne adayının transferden sadece bir gün öncesinde Kıbrıs’ta olması yeterlidir. Sperm donasyonunda ise anne adayının yumurtaları toplanmadan (OPU) sadece bir gün önce Kıbrıs’ta olması yeterlidir. Tedavi embriyo donasyonu olacak ise çiftin yine transfer tarihinden sadece bir gün önce Kıbrıs’ta olması yeterlidir."
+        desc: ""
     },
     3: {
         title: "Seyahat",
-        desc: "Kıbrıs’ ta tüp bebek tedavisi için ulaşım yolu genellikle uçak seyahatidir. Deniz yoluyla Türkiye üzerinden deniz otobüsü ile 2-3 saatlik yolculukla da ulaşmak mümkündür. Hava yoluyla Lefkoşa Ercan Havalimanı veya Larnaka havalimanından ulaşım kolaylıkla sağlanabilir. Avrupa üzerinden gelecek olan yolcularımız için Larnaka havalimanına direk uçuşlar bulunmaktadır. Seyahatin en başından sonuna kadar her noktasında merkezimiz istendiği takdirde yardım sağlamaktadır. Havalimanı ulaşımları, uçak biletleri, otelden tüp bebek merkezine ulaşım, turistik amaçlı geziler dahil her türlü ulaşım imkanı sağlanabilir."
+        desc: ""
     },
     4: {
         title: "Konaklama",
-        desc: "Kıbrıs’ta tüp bebek tedavisi sırasında konaklama için oldukça fazla seçenek mevcuttur. Merkeze yürüme mesafesinde beş yıldızlı oldukça konforlu oteller olabileceği gibi, oldukça uygun fiyatlı butik oteller de mevcuttur. Tüp bebek tedavisinin Kıbrıs’ta yapılması tercih etmek anne ve baba adayının stresini azaltır. İklimin hemen hemen her mevsim ılıman olduğu bu güzel adada tatil yaparken bebek sahibi olabilirsiniz."
+        desc: ""
     },
     5: {
         title: "Tedavi",
-        desc: "Kıbrıs’ta Tüp Bebek Tedavisi seçilecek yönteme göre değişkenlik gösterse de tedavilerin oldukça geniş açıklaması ana sayfamızda yer almaktadır. Daha fazla bilgi ve ayrıntı için bizimle iletişime geçebilirsiniz."
+        desc: ""
     },
     6: {
         title: "Gebelik Testi",
-        desc: "Kıbrıs’ta tüp bebek tedavisi sonrası çiftlerin çoğu transferden bir gün sonra kendi yaşadıkları ülkeye dönmektedir. Bizim önerimiz transferden 12 gün sonra kanda gebelik testi (b-HCG ) yapılmasıdır. Öncesinde yapılan idrar veya kan testleri yanıltıcı olabilmektedir."
+        desc: ""
     }
 };
